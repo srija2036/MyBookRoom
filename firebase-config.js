97a17f47426034248f68c1e3acb0bbd68a9aea50
srija2036 <srijasura6789@gmail.com>
2025-12-26 18:34:11 +0530

    Authentication i tried

--- firebase-config.js
+++ firebase-config.js
@@ -2,14 +2,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
- const firebaseConfig = {
-    apiKey: "AIzaSyCvuo23eLJLLOKnHCCTT270F3xPWVc-r4o",
-    authDomain: "mybookroom-a0ba7.firebaseapp.com",
-    projectId: "mybookroom-a0ba7",
-    storageBucket: "mybookroom-a0ba7.firebasestorage.app",
-    messagingSenderId: "217478922812",
-    appId: "1:217478922812:web:61b6d30bcdec863783b939"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCvuo23eLJLLOKnHCCTT270F3xPWVc-r4o",
+  authDomain: "mybookroom-a0ba7.firebaseapp.com",
+  projectId: "mybookroom-a0ba7",
+  storageBucket: "mybookroom-a0ba7.firebasestorage.app",
+  messagingSenderId: "217478922812",
+  appId: "1:217478922812:web:61b6d30bcdec863783b939"
+};
 
 
 const app = initializeApp(firebaseConfig);
